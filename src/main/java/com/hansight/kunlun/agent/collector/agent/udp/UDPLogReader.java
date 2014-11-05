792d5dbafeeb12485ed5d937b30f949e878f198e
lumeng_baichao@hansight.com <lumeng_baichao@hansight.com>
2014-11-05 15:49:14 +0800

    delete

--- src/main/java/com/hansight/kunlun/agent/collector/agent/udp/UDPLogReader.java
+++
@@ -1,7 +0,0 @@
-package com.hansight.kunlun.agent.collector.agent.udp;
-
-import com.hansight.kunlun.agent.collector.agent.reader.DefaultLogReader;
-
-public abstract class UDPLogReader extends DefaultLogReader {
-
-}
--- src/main/java/com/hansight/kunlun/agent/collector/coordinator/config/ConfigCategory.java
+++
@@ -1,6 +0,0 @@
-package com.hansight.kunlun.agent.collector.coordinator.config;
-
-public enum ConfigCategory {
-	AGENT,
-	FORWARDER
-}
